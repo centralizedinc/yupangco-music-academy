6a53363be53196dfafd04ccef900812677b5b4cb
Mark Jhon Paul Quijom <markjhonpaul.quijom@gmail.com>
2019-08-01 16:30:38 +0800

    update

--- api/routes/fb_setup_router.js
+++ api/routes/fb_setup_router.js
@@ -124,7 +124,6 @@ function setupPersistentMenu(callback) {
     ChatbotResponseDao.findOne({
         postback: "PERSISTENT_MENU"
     }).then((persistent_menu) => {
-        console.log('persistent_menu :', persistent_menu);
         return axios.post(
             constants_helper.fb_messenger_profile + "?" + qs.stringify({ access_token: constants_helper.fb_token }),
             (persistent_menu && persistent_menu.message) ? persistent_menu.message : json
