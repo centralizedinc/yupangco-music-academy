a4835f92f133ee3736107f3de36a8315b156e060
Mark Jhon Paul Quijom <markjhonpaul.quijom@gmail.com>
2019-08-02 11:03:08 +0800

    test details

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ var app = express();
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({'extended':'false'}));
-app.use(express.static(path.join(__dirname, 'web/dist')));
+app.use(express.static(path.join(__dirname, 'dist')));
 app.set('view engine', 'html');
 
 // SETUP CORS
