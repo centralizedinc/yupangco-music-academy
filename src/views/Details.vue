a69191c1587d3ed67357bfebb5f333bd957fb7d4
Mark Jhon Paul Quijom <markjhonpaul.quijom@gmail.com>
2019-08-05 13:05:19 +0800

    update

--- src/views/Details.vue
+++ src/views/Details.vue
@@ -1,17 +1,21 @@
 <template>
   <div>
     <a-row>
-      <a-col :span="24">
+      <a-col :span="24" v-if="reminder">
+        <guitar-details v-if="course===5" />
+      </a-col>
+      <a-col :span="24" v-else>
         <guitar-details v-if="course===5" />
       </a-col>
       <a-col :span="24">
-        <a-button 
-          type="primary" 
-          :disabled="lesson===null" 
+        <a-button
+          type="primary"
+          :disabled="lesson===null"
           block
-          @click="enroll">
-          Enroll Now !
-        </a-button>
+          v-if="reminder"
+          @click="submit"
+        >Enroll Now !</a-button>
+        <a-button type="primary" :disabled="lesson===null" block v-else @click="enroll">Continue</a-button>
       </a-col>
     </a-row>
   </div>
@@ -25,23 +29,39 @@ export default {
   },
   data() {
     return {
-      course: null
+      reminder: false
     };
   },
   computed: {
     lesson() {
       return this.$store.state.lesson;
+    },
+    level() {
+      return parseInt(this.$route.query.level);
+    },
+    course() {
+      return parseInt(this.$route.query.course);
     }
-  },
-  created() {
-    console.log("this.$route.query :", this.$route.query);
-    this.course = parseInt(this.$route.query.course);
   },
   methods: {
-    enroll(){
-      this.$router.push({ path: 'enroll', query: this.$route.query })
+    submit() {
+      // this.$router.push({ path: 'enroll', query: this.$route.query })
+      if (parseInt(this.lesson) === 0) this.enroll();
+      else this.reminder = true;
+    },
+    enroll() {
+      this.$store.dispatch("ENROLLMENT", {
+        details: {
+          course: this.course,
+          level: this.level
+        },
+        sender: this.$route.query.sender,
+        postback: `CALLBACK_ENROLLMENT_#${this.getCourse(
+          this.course
+        )}_${this.getLevel(this.level)}_${this.getLesson(this.lesson)}`
+      });
     }
-  },
+  }
 };
 </script>
 
