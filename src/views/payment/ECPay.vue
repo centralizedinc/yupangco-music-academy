c3db8875a461c614d1d4be1245279cfac5ccf344
Mark Jhon Paul Quijom <markjhonpaul.quijom@gmail.com>
2019-08-05 13:41:16 +0800

    update

--- src/views/payment/ECPay.vue
+++ src/views/payment/ECPay.vue
@@ -1,49 +1,56 @@
 <template>
-    <a-card 
-        title="Payment via ECPay"
-        :headStyle="card_head_style"
-        :bodyStyle="card_body_style">
-        <p style="font-weight:bold;">
-            Your Reference Number:
-        </p>
-        <p style="text-align: center; background: lightblue; font-weight:bold;margin: 10px; font-size: 36px">
-            {{reference_number}}
-        </p>
-        <p style="font-weight:bold;text-align: left">General Guidelines:</p>
-        <ul style="text-align: left">
-            <li>Reference number can only be used once. If you made a short payment by mistake, do not try to correct it by making another bills payment with the same reference number.</li>
-            <li>Amount is inclusive of convenience fee/ If you are paying for multiple reference numbers, pay separately for each reference number. Only one (1) transaction per reference number.</li>
-            <li>The payment of fee is not a guarantee that the application will be granted. The processing of the application will still be subject to the evaluation of the concerned YMA personnel and its compliance with the pertinent laws, rules and regulations</li>
-        </ul>
-        <a-button type="primary" @click="close" block>CLOSE</a-button>
-    </a-card>
+  <a-card title="Payment via ECPay" :headStyle="card_head_style" :bodyStyle="card_body_style">
+    <p style="font-weight:bold;">Your Reference Number:</p>
+    <p
+      style="text-align: center; background: lightblue; font-weight:bold;margin: 10px; font-size: 36px"
+    >{{reference_number}}</p>
+    <p style="font-weight:bold;text-align: left">General Guidelines:</p>
+    <ul style="text-align: left">
+      <li>Reference number can only be used once. If you made a short payment by mistake, do not try to correct it by making another bills payment with the same reference number.</li>
+      <li>Amount is inclusive of convenience fee/ If you are paying for multiple reference numbers, pay separately for each reference number. Only one (1) transaction per reference number.</li>
+      <li>The payment of fee is not a guarantee that the application will be granted. The processing of the application will still be subject to the evaluation of the concerned YMA personnel and its compliance with the pertinent laws, rules and regulations</li>
+    </ul>
+    <a-button type="primary" @click="close" block>CLOSE</a-button>
+  </a-card>
 </template>
 
 <script>
 export default {
-    computed: {
-        reference_number(){
-            var d = new Date()
-            return d.getFullYear().toString() + (d.getMonth() + 1).toString() + this.random_number
-        },
-        random_number(){
-            return Math.floor(Math.random() * 9999);
-        },
-        card_head_style() {
-        return { background: "#F2E9C8", "font-weight": "bold" };
-        },
-        card_body_style() {
-        return { "text-align": "left" };
-        }
+  created() {
+    console.log(
+      "this.$store.state.details :",
+      JSON.stringify(this.$store.state.details)
+    );
+  },
+  computed: {
+    reference_number() {
+      var d = new Date();
+      return (
+        d.getFullYear().toString() +
+        (d.getMonth() + 1).toString() +
+        this.random_number
+      );
     },
-    methods: {
-        close(){
-            this.$store.dispatch("CLOSE", {sender: this.$route.query.sender, postback: "CB_PAYMENT"})
-        }
+    random_number() {
+      return Math.floor(Math.random() * 9999);
     },
-}
+    card_head_style() {
+      return { background: "#F2E9C8", "font-weight": "bold" };
+    },
+    card_body_style() {
+      return { "text-align": "left" };
+    }
+  },
+  methods: {
+    close() {
+      this.$store.dispatch("CLOSE", {
+        sender: this.$route.query.sender,
+        postback: "CB_PAYMENT"
+      });
+    }
+  }
+};
 </script>
 
 <style>
-
 </style>
